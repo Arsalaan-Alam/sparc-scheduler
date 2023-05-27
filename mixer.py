ecd1df59cf33b4baef9792ca8e13e966175cb63a
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-05-27 12:48:04 +0530

    mixer.py 50% working

--- mixer.py
+++ mixer.py
@@ -1,29 +1,25 @@
-import random
 import json
 
-class_names = []
-time_slots = ["10:30 AM - 11:30 AM", "11:30 AM - 12:30 PM", "12:30 PM - 1:30 PM"]
 students = ["4618", "5222", "4058", "9022", "3292", "5474", "4834", "2190", "9763", "1249", "8952", "2895"]
-
-# Prompt for class names
-for i in range(3):
-    class_name = input(f"Enter the name of class {i+1}: ")
-    class_names.append(class_name)
+time_slots = ["10:30 AM - 11:30 AM", "11:30 AM - 12:30 PM", "12:30 PM - 1:30 PM"]
 
 schedule = []
 
-# Create the schedule for each time slot
-for time_slot in time_slots:
+class_names = []
+for _ in range(3):
+    class_name = input("Enter the name of the class: ")
+    class_names.append(class_name)
+
+class_index = 0
+for _ in range(3):
     classes = []
 
-    # Assign students to classes
-    for class_name in class_names:
-        if len(students) < 4:
-            print("Error: Insufficient number of students available.")
-            exit(1)
+    for _ in range(3):
+        class_name = class_names[class_index]
 
-        eligible_students = random.sample(students, 4)
-        students = [student for student in students if student not in eligible_students]
+        # Select four students for the class
+        eligible_students = students[:4]
+        students = students[4:]
 
         class_info = {
             "className": class_name,
@@ -31,15 +27,13 @@ for time_slot in time_slots:
         }
         classes.append(class_info)
 
+        class_index = (class_index + 1) % 3
+
     time_slot_info = {
-        "time": time_slot,
+        "time": time_slots.pop(0),
         "classes": classes
     }
     schedule.append(time_slot_info)
-
-# Repeat the schedule for 2 more times
-for i in range(2):
-    schedule.extend(schedule)
 
 # Convert the data to JSON
 json_data = json.dumps(schedule, indent=4)
