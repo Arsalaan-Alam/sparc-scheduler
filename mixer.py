99ce5d669dbe573ef68134ef4a1a366b6a2fc0a7
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-05-27 12:46:28 +0530

    updating mixer.py

--- mixer.py
+++ mixer.py
@@ -1,18 +1,29 @@
+import random
 import json
 
-students = ["4618", "5222", "4058", "9022", "3292", "5474", "4834", "2190", "9763", "1249", "8952", "2895"]
+class_names = []
 time_slots = ["10:30 AM - 11:30 AM", "11:30 AM - 12:30 PM", "12:30 PM - 1:30 PM"]
+students = ["4618", "5222", "4058", "9022", "3292", "5474", "4834", "2190", "9763", "1249", "8952", "2895"]
+
+# Prompt for class names
+for i in range(3):
+    class_name = input(f"Enter the name of class {i+1}: ")
+    class_names.append(class_name)
 
 schedule = []
 
+# Create the schedule for each time slot
 for time_slot in time_slots:
     classes = []
 
-    for i in range(3):
-        class_name = input(f"Enter the name of class {i+1}: ")
+    # Assign students to classes
+    for class_name in class_names:
+        if len(students) < 4:
+            print("Error: Insufficient number of students available.")
+            exit(1)
 
-        # Select four students for the class
-        eligible_students = students[i*4:(i+1)*4]
+        eligible_students = random.sample(students, 4)
+        students = [student for student in students if student not in eligible_students]
 
         class_info = {
             "className": class_name,
@@ -26,11 +37,15 @@ for time_slot in time_slots:
     }
     schedule.append(time_slot_info)
 
+# Repeat the schedule for 2 more times
+for i in range(2):
+    schedule.extend(schedule)
+
 # Convert the data to JSON
 json_data = json.dumps(schedule, indent=4)
 
 # Save the JSON data to a file
-with open('schedule1.json', 'w') as file:
+with open('schedule.json', 'w') as file:
     file.write(json_data)
 
 print("JSON data has been generated and saved to 'schedule.json'.")
